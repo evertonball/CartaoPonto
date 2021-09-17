92612a61f392664df3a7d4c83b7fb4354cfc8fdf
evertonball <evertonball@gmail.com>
2021-09-17 15:51:35 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/dio/CartaoPonto/CartaoPontoApplication.java
@@ -0,0 +1,18 @@
+package com.dio.CartaoPonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class CartaoPontoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CartaoPontoApplication.class, args);
+		System.out.println("Teste");
+
+	}
+
+}
---
+++ src/test/java/com/dio/CartaoPonto/CartaoPontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.CartaoPonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartaoPontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
